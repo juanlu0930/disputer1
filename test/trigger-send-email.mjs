19e9ae0ae3d49b6f0bad560d5997887a7cde2e9b
juanl <juanl@squareup.com>
2022-03-29 22:03:34 +0800

    debug buildkite

--- test/trigger-send-email.mjs
+++ test/trigger-send-email.mjs
@@ -5,7 +5,7 @@ import {testData} from "./support/config.mjs"
 import { expect } from "chai"
 import { config } from "dotenv"; config()
 
-describe("Get Features By Name Test", function() {
+describe("Trigger to send email Test", function() {
 
     describe ("Get Features by Name should return the correct value",  function() {
         // this.retries(4)
@@ -16,7 +16,7 @@ describe("Get Features By Name Test", function() {
             //Trigger to send email
             sendEmail = await triggerSendEmail("https://dispute.risk.us.platform.afterpay-beta.com/manual/manually_send_add_card_email",
                 axiosOptions)
-            expect(sendEmail.data.message).to.be.equals("Send add_card emails successfully.")
+            expect(sendEmail.status).to.be.equals(200)
         })
     })
 })
